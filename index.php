bd5d9004002c94b362d5908c9cbb28765a983c0e
yoimuri <145955894+yoimuri@users.noreply.github.com>
2024-11-25 13:17:20 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+   <head>
+        <title>CCC321-18/CCL321-18</title>
+   </head>
+   <body>
+   <center>
+   <hr/>
+   <h1>CCL321-18 Application Development & Emerging Technologies (Lab)</h1>
+   <h3>Section: 3BSCS-2</h3>
+   <hr/>
+   <img src="poyaoan.jpg" width="200" height="200"/>
+   <p>Name:Clint Branwel D. Poyaoan</p>
+   <h3>BOOTSTRAP ACTIVITIES</h3>
+   
+         <a href="Activity1">Activity1</a>
+		 <br/>
+		 <a href="Activity2">Activity2</a>
+		 <br/>
+		 <a href="Activity3">Activity3</a>
+         <br/>
+		 <a href="Activity4">Activity4</a>
+         <br/>
+		 <a href="Activity5">Activity5</a>
+         <br/>
+		 <a href="Activity6">Activity6</a>
+         <br/>
+		 <a href="Portfolio">Portfolio</a>
+		 <br/>
+		 <a href="photo_gallery">Photo Gallery</a>
+		 <br/>
+	     <a href="blog_project">Blog</a>
+		 <br/>
+	     <a href="e_commerce">E-Commerce</a>
+   </body>
+   </center>
+
+</html>
